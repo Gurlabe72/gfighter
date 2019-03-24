06ffac127c9f5b25f409c74ebad418c2a6b0282e
Gurlabe <gurlabe72@gmail.com>
2019-03-23 17:34:53 -0700

    USERS data base created

--- server/index.js
+++ server/index.js
@@ -4,31 +4,48 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/test5");
 
 const User = mongoose.model("User", {
-  text: String,
+  name: String,
+  bio: String,
+  style: String,
+  image_url: String,
+  strength: String,
   complete: Boolean
 });
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    users: [User]
   }
   type User {
       id: ID!
-text: String!
+name: String!
+bio: String!
+style: String!
+image_url: String!
+strength: String!
 complete: Boolean!  
   }
   type Mutation {
-      createUser( text: String!): User
+      createUser( name: String!, bio: String!, style: String!, image_url: String!, strength: String! ): User
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`
+    hello: (_, { name }) => `Hello ${name || "World"}`,
+    users: () => User.find()
   },
   Mutation: {
-    createUser: async (_, { text }) => {
-      const user = new User({ text, complete: false });
+    createUser: async (_, { name, bio, style, image_url, strength }) => {
+      const user = new User({
+        name,
+        bio,
+        style,
+        image_url,
+        strength,
+        complete: false
+      });
       await user.save();
       return user;
     }
