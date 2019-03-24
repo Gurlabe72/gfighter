d085631665a88be575881e4b3af4654c59b14844
Gurlabe <gurlabe72@gmail.com>
2019-03-23 17:00:35 -0700

    beginning User backend set up

--- server/index.js
+++ server/index.js
@@ -1,16 +1,41 @@
 const { GraphQLServer } = require("graphql-yoga");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/test5");
+
+const User = mongoose.model("User", {
+  text: String,
+  complete: Boolean
+});
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
+  }
+  type User {
+      id: ID!
+text: String!
+complete: Boolean!  
+  }
+  type Mutation {
+      createUser( text: String!): User
   }
 `;
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || "World"}`
+  },
+  Mutation: {
+    createUser: async (_, { text }) => {
+      const user = new User({ text, complete: false });
+      await user.save();
+      return user;
+    }
   }
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log("Server is running on localhost:4000"));
+mongoose.connection.once("open", function() {
+  server.start(() => console.log("Server is running on localhost:4000"));
+});
